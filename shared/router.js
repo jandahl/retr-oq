4a6e95c11093aeaadabe7c41b90aecb6cfa65dae
Jan Gronemann <jandahl@users.noreply.github.com>
2026-09-04 00:05:36 +0200

    Cache-bust screensaver.js to v=15

--- shared/router.js
+++ shared/router.js
@@ -78,8 +78,8 @@
     const s = document.createElement("script");
     const src = document.currentScript && document.currentScript.src;
     s.src = src
-      ? src.replace(/router\.js.*$/, "redmond/screensaver.js?v=14")
-      : "../shared/redmond/screensaver.js?v=13";
+      ? src.replace(/router\.js.*$/, "redmond/screensaver.js?v=15")
+      : "../shared/redmond/screensaver.js?v=15";
     document.head.appendChild(s);
   }
 })();
